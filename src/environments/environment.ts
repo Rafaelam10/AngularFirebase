2832c07cd878eed387b69311b4a47722cd9d0299
Rafaelam10 <rafinha1.matos@gmail.com>
2020-12-11 22:04:40 -0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyC5qVv-UbIM-GLdgNbyTScy0oDzmQ_tPBU",
+  authDomain: "algularfire.firebaseapp.com",
+  projectId: "algularfire",
+  storageBucket: "algularfire.appspot.com",
+  messagingSenderId: "275055474343",
+  appId: "1:275055474343:web:04134eb705c099f851cd82",
+  measurementId: "G-SD2EFWVGX1"
+}
+}
